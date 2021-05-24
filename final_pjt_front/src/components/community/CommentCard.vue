541e6a85f6ed060bf0c8a0c9b33915868e56aa1a
sangwook0613 <kvmoke@gmail.com>
2021-05-24 09:05:10 +0900

    Update front comment card

--- final_pjt_front/src/components/community/CommentCard.vue
+++ final_pjt_front/src/components/community/CommentCard.vue
@@ -1,15 +1,30 @@
 <template>
   <div>
     <div class="card" v-for="(comment, idx) in comments" :key="idx">
-      {{ comment }}
+      <div class="d-flex">
+        <div class="p-3">{{ comment.user }}</div>
+        <div class="p-3">{{ comment.content }}</div>
+        <button v-if="comment.user === jwtUserId" class="btn btn-danger">삭제</button>
+      </div>
     </div>
-    <h1>hihi</h1>
   </div>
 </template>
 
 <script>
+import { mapGetters } from 'vuex'
+
 export default {
   name: 'CommentCard',
+  props: {
+    comments: {
+      type: Array,
+    }
+  },
+  computed: {
+    ...mapGetters([
+      'jwtUserId',
+    ])
+  }
 }
 </script>
 
