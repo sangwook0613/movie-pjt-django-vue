518aa8e92087905b9abe3343dbae0c1282d1de50
sangwook0613 <kvmoke@gmail.com>
2021-05-26 13:26:47 +0900

    Design front movie page

--- final_pjt_back/make_csv.py
+++ final_pjt_back/make_csv.py
@@ -1,22 +1,12 @@
 import os, django
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'server.settings')
 django.setup()
-# from json import JSONDecodeError
-# import requests
-# import datetime
-# from bs4 import BeautifulSoup
-# from pprint import pprint
-# from decouple import config
 
 from movies.models import Genre, Movie, Keyword, Person
 import csv
-import datetime
-from django.http import HttpResponse
 
 movie_data = open('write.csv','w', newline='')
 writer = csv.writer(movie_data)
-# wr.writerow([1,'림코딩', '부산'])
-# wr.writerow([2,'김갑환', '서울'])
  
 # 컬럼명 생성
 column_names = ['id']
@@ -26,7 +16,7 @@ for genre in Genre.objects.all():
 for keyword in Keyword.objects.all():
     column_names.append(f'k_{keyword.id}')
 print(len(column_names))
-# print(column_names)
+
 writer.writerow(column_names)
 
 cnt = 0
@@ -65,3 +55,14 @@ movie_data.close()
 # return response
     
 
+
+# cut_num = 10
+# cnt = 0
+# for keyword in Keyword.objects.all():
+#     if keyword.keyword_count < cut_num:
+#         cnt += 1
+#         keyword.delete()
+#     if cnt % 100 == 0:
+#         print(cnt)
+
+# print(cnt)
--- final_pjt_front/src/components/movies/MovieDetailReviews.vue
+++ final_pjt_front/src/components/movies/MovieDetailReviews.vue
@@ -1,31 +1,69 @@
 <template>
-  <div class="container row d-flex justify-content-center">
-    <div class="col-6 card-body h-50" v-for="(review, idx) in reviews.slice(0, 4)" :key="idx">
-      <div class="card p-2 review-short-card">
-        <div class="d-flex justify-content-between">
-          <span class="fw-bold fs-5 pe-2">
-            <router-link
-              :to="{ name: 'ReviewDetail', params: { movieId: review.movie, reviewId: review.id }}"
-              class="text-decoration-none text-dark text-end"
-            >
-              {{ review.title }}
-            </router-link>
-          </span>
-          <button :id="`likeCount-${review.id}`" class="btn" @click="updateReviewLikes(review, review.user)">
-            <i class="fas fa-heart fa-lg me-1" :style="{ color: checkUserIncludeInReviewLikes(review.likes)}"></i>
-            <span>{{ review.likes.length }}</span>
-          </button>
-        </div>
-        <div class="d-flex justify-content-between">
-          <span :class="[makeBadge(review.rating), 'text-center']" style="width: 80px">평점: {{ review.rating }}</span>
-          <!-- <router-link
+  <div class="container px-4">
+    <div class="row d-flex justify-content-center g-2">
+      <div class="col-xl-3 card-body h-50 component-1" v-for="(review, idx) in reviews.slice(0, 4)" :key="idx">
+        <div class="d-flex flex-column align-items-start">
+          <router-link
             :to="{ name: 'ReviewDetail', params: { movieId: review.movie, reviewId: review.id }}"
-            class="text-decoration-none text-dark text-end"
+            class="text-decoration-none text-dark"
           >
-            리뷰 상세보기
-          </router-link> -->
-          <span>{{ review.user }}</span>
+            <div class=" d-flex justify-content-start">
+              <img
+                alt="profile-image"
+                class="ellipse-2"
+                src="https://static.overlay-tech.com/assets/eba0d02d-858f-4cab-9e4e-d897a0d4800d.png"
+              />
+              <div>
+                <p class="username">usernamegoeshere</p>
+                <p class="rating">평점 {{ review.rating }}</p>
+              </div>
+            </div>
+            <div class="mb-3">
+              <div class="title fw-bold">
+                러빙 빈센트는 진짜 그림이 다했다 {{ review.title }}
+              </div>
+              <div class="content">
+                얼씨구절씨구얼씨구절씨구얼씨구절씨얼씨구절씨구얼씨구절씨구얼씨구절씨구얼씨구...
+              </div>
+              <div class="rating">&#43; 더보기</div>
+            </div>
+          </router-link>
+          <div class="d-flex align-items-center">
+            <div :id="`likeCount-${review.id}`" class="" @click="updateReviewLikes(review, review.user)">
+              <i class="fas fa-heart fa-lg me-1" :style="{ color: checkUserIncludeInReviewLikes(review.likes)}"></i>
+              <span class="ps-2 rating">{{ review.likes.length }}</span>
+            </div>
+            <div class="comment-count">
+                <img
+                  alt=""
+                  class="vector"
+                  src="https://static.overlay-tech.com/assets/6d5c72bb-4b13-4f8a-99e7-fc4b3a1c9049.svg"
+                />
+              <!-- comment 수 필요 -->
+              <span class="ps-2 rating">5</span>
+            </div>
+          </div>
         </div>
+        <!-- <div class="card p-2 review-short-card">
+          <div class="d-flex justify-content-between">
+            <span class="fw-bold fs-5 pe-2">
+              <router-link
+                :to="{ name: 'ReviewDetail', params: { movieId: review.movie, reviewId: review.id }}"
+                class="text-decoration-none text-dark text-end"
+              >
+                {{ review.title }}
+              </router-link>
+            </span>
+            <button :id="`likeCount-${review.id}`" class="btn" @click="updateReviewLikes(review, review.user)">
+              <i class="fas fa-heart fa-lg me-1" :style="{ color: checkUserIncludeInReviewLikes(review.likes)}"></i>
+              <span>{{ review.likes.length }}</span>
+            </button>
+          </div>
+          <div class="d-flex justify-content-between">
+            <span :class="[makeBadge(review.rating), 'text-center']" style="width: 80px">평점: {{ review.rating }}</span>
+            <span>{{ review.user }}</span>
+          </div>
+        </div> -->
       </div>
     </div>
     <!-- {{ reviews[0].movie}} -->
@@ -126,7 +164,7 @@ export default {
 </script>
 
 <style scoped>
-.review-card {
+/* .review-card {
   position: relative;
   display: inline-block;
   flex-direction: column;
@@ -150,5 +188,64 @@ export default {
 .review-short-card {
   background-color: rgba(41, 146, 152, 1);
   border: 3px solid rgba(34, 41, 66, 0.9);
+} */
+
+.component-1 {
+  background-color: #292828;
+  border-radius: 8px;
+}
+/* .frame-11 {
+  width: calc(100% - 48px);
+  height: calc(100.73% - 48px);
+  padding: 24px;
+  position: absolute;
+  left: 0;
+  top: -1px;
+} */
+.frame-10 {
+  margin-bottom: 24px;
+}
+.ellipse-2 {
+  width: 48px;
+  height: 48px;
+  margin-right: 15px;
+  border-radius: 50%;
+}
+.frame-7 {
+  padding: 0 44px 0 0;
+}
+.username {
+  color: #FFFFFF;
+  margin-bottom: 4px;
+  @include noto-sans-kr-14-bold;
+}
+.rating {
+  color: #FFFFFF;
+  @include noto-sans-kr-14-regular;
+}
+.title {
+  color: #FFFFFF;
+  margin-bottom: 14px;
+  @include noto-sans-kr-14-bold;
+}
+.content {
+  color: #FFFFFF;
+  margin-bottom: 14px;
+  @include noto-sans-kr-14-regular;
+}
+.frame-12 {
+  margin-right: 14px;
+}
+.favorite-border-black-24dp-1 {
+  margin-right: 8px;
+  padding: 3px 2px 2.65px;
+}
+.vector {
+  width: 50%;
+  height: 50%;
+}
+.comment-count {
+  margin-left: 10px;
+  /* padding: 2px; */
 }
 </style>
